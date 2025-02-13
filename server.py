48a620b35d5ee290d3e4f6968767b1e66e221ba2
RaulHernandez19 <163434295+RaulHernandez19@users.noreply.github.com>
2025-02-13 11:27:39 -0800

    server fixv2

--- server.py
+++ server.py
@@ -130,4 +130,5 @@ def header():
     return render_template("header.html") 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que Render asigna
+    app.run(host="0.0.0.0", port=port, debug=True)
